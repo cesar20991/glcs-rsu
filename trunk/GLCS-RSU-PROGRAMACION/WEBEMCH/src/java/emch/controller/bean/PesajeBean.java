e45329eee91cb1f3a5d8ac72c392b8be58215c64
diegopa11.11@gmail.com <diegopa11.11@gmail.com@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-05-29 09:57:48 +0000

    El BEAN de Pesaje

---
+++ trunk/GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/PesajeBean.java
@@ -0,0 +1,44 @@
+package emch.controller.bean;
+
+
+import emch.modelo.acceso.*;
+import emch.modelo.entidades.*;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean
+@SessionScoped
+public class PesajeBean {
+    private TLiquidacion selectedLiquidacion;
+    private List<TPesaje> listarPesaje;
+    
+    
+    @PostConstruct
+    public void init() {
+        
+    }
+
+    public TLiquidacion getSelectedLiquidacion() {
+        return selectedLiquidacion;
+    }
+
+    public void setSelectedLiquidacion(TLiquidacion selectedLiquidacion) {
+        this.selectedLiquidacion = selectedLiquidacion;
+    }
+
+    public List<TPesaje> getListarPesaje() {
+        PesajeManaged objPes = new PesajeManaged();
+        listarPesaje = objPes.listarPesaje(selectedLiquidacion);
+        return listarPesaje;
+    }
+
+    public void setListarPesaje(List<TPesaje> listarPesaje) {
+        this.listarPesaje = listarPesaje;
+    }
+
+    
+       
+}
