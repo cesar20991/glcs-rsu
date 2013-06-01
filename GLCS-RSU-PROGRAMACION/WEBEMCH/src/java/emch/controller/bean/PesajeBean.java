d9d0c83701d3bc9c9acb70aa6ff55c4bd4b9ae4a
cesar20991 <cesar20991@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-06-01 05:22:35 +0000

    se arreglo lo de usuarios

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/PesajeBean.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/PesajeBean.java
@@ -11,7 +11,7 @@ import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
-public class PesajeBean {
+public class PesajeBean extends UsuarioBean {
     private TLiquidacion selectedLiquidacion;
     private List<TPesaje> listarPesaje;
     private List<TComprobante> listarComprobante;
