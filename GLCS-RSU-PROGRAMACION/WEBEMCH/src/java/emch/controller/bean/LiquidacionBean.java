dddec9b574842f6b458d4919838ad4f89dd93909
cesar20991 <cesar20991@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-06-02 22:00:18 +0000

    50% del CUS generarliquidacion, Falta modificar.

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/LiquidacionBean.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/LiquidacionBean.java
@@ -6,9 +6,16 @@ package emch.controller.bean;
 
 import emch.modelo.acceso.LiquidacionManaged;
 import emch.modelo.entidades.*;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 /**
  *
@@ -16,16 +23,18 @@ import javax.faces.bean.RequestScoped;
  */
 @ManagedBean
 @RequestScoped
-public class LiquidacionBean extends UsuarioBean{
+public class LiquidacionBean extends UsuarioBean {
 
     private TLiquidacion liquidacion;
     private List<TLiquidacion> listaliquidacion;
     private List<TTipoliquidacion> listatipoliq;
     private List<TPesaje> listadopesaje;
-     private TPesaje[] selectedpesaje;
-               
+    private TPesaje[] selectedpesaje;
+    private Date fechaInicio;
+    private Date fechaFin;
+
     public LiquidacionBean() {
-        liquidacion = new TLiquidacion(); 
+        liquidacion = new TLiquidacion();
         liquidacion.setTTipoliquidacion(new TTipoliquidacion());
         liquidacion.setTEmpresa(new TEmpresa());
     }
@@ -55,12 +64,16 @@ public class LiquidacionBean extends UsuarioBean{
     public void setListatipoliq(List<TTipoliquidacion> listatipoliq) {
         this.listatipoliq = listatipoliq;
     }
-    
-    public String irAperturar(){
+
+    public String irAperturar() { //throws IOException {
+        // FacesContext.getCurrentInstance().getExternalContext().redirect("generarliquidacion.xhtml");
         return "generarliquidacion";
     }
 
     public List<TPesaje> getListadopesaje() {
+        LiquidacionManaged obj = new LiquidacionManaged();
+        listadopesaje = obj.listarPesajePendiente(getEmpresa().getCdRuc(), fechaInicio, fechaFin);
+
         return listadopesaje;
     }
 
@@ -72,8 +85,69 @@ public class LiquidacionBean extends UsuarioBean{
         return selectedpesaje;
     }
 
+    public void demo(ActionEvent evt) {
+    }
+
     public void setSelectedpesaje(TPesaje[] selectedpesaje) {
         this.selectedpesaje = selectedpesaje;
     }
 
+//    public void irListarPesajes(ActionEvent event) {
+//        LiquidacionManaged obj = new LiquidacionManaged();
+//        listadopesaje = obj.listarPesajePendiente(getEmpresa().getCdRuc(), fechaInicio,fechaFin);
+//    }
+    public void irCalcularTN() {
+        BigDecimal SUMAtotalTN = new BigDecimal("0.000").setScale(2);        
+        for (int i = 0; i < selectedpesaje.length; i++) {            
+            SUMAtotalTN = SUMAtotalTN.add(selectedpesaje[i].getPesoTn());
+        }
+        liquidacion.setTotalTn(SUMAtotalTN);
+    }
+
+    public String insertar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        irCalcularTN();
+        liquidacion.setTEmpresa(getEmpresa());
+        liquidacion.setUsuCrea(getUsuario2().getNomUsu());
+        liquidacion.setFechaReg(liquidacion.getFechaApertura());
+        LiquidacionManaged visitaMgd = new LiquidacionManaged();
+        boolean resultado = visitaMgd.IngresarLiquidacion(liquidacion,selectedpesaje);
+        if (resultado) {
+            context.addMessage(null, new FacesMessage("Liquidacion Generada Correctamente", "Verificar"));
+            return "liquidaciones";
+        } else {
+            //setEsEdicion(false);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Generar Liquidacion", "Verificar"));
+            return ""; //futuros errores
+        }
+        //return "";
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 }
--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/LiquidacionManaged.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/LiquidacionManaged.java
@@ -1,12 +1,19 @@
 package emch.modelo.acceso;
 
+import emch.modelo.entidades.TEstadoliq;
+import emch.modelo.entidades.TEstadoxliquidacion;
+import emch.modelo.entidades.TEstadoxliquidacionId;
 import emch.modelo.entidades.TLiquidacion;
+import emch.modelo.entidades.TPesaje;
 import emch.modelo.entidades.TTipoliquidacion;
 import emch.modelo.entidades.TTipotrabajador;
 import emch.modelo.entidades.TTrabajador;
 import emch.modelo.entidades.TUbigeo;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import org.hibernate.Query;
 import org.hibernate.Transaction;
 import org.hibernate.classic.Session;
@@ -20,32 +27,128 @@ public class LiquidacionManaged {
 
     public List listarLiquidacion() {
         List<TLiquidacion> listaLiq = null;
-      /*  try {*/
-            sesion = HibernateUtil.getSessionFactory().openSession();
-            qry = sesion.createQuery("FROM TLiquidacion");
-            listaLiq = (List<TLiquidacion>) qry.list();
-       /* } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            sesion.close();
-        }*/
+        /*  try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        qry = sesion.createQuery("FROM TLiquidacion");
+        listaLiq = (List<TLiquidacion>) qry.list();
+        /* } catch (Exception ex) {
+         ex.printStackTrace();
+         } finally {
+         sesion.close();
+         }*/
         return listaLiq;
     }
-    
+
     public List ListarTipoLiquidacion() {
         List<TTipoliquidacion> listaTLiq = null;
-      /*  try {*/
-            sesion = HibernateUtil.getSessionFactory().openSession();           
-            qry = sesion.createQuery("FROM TTipoliquidacion");            
-            listaTLiq = (List<TTipoliquidacion>) qry.list();
-       /* } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            sesion.close();
-        }*/
+        /*  try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        qry = sesion.createQuery("FROM TTipoliquidacion");
+        listaTLiq = (List<TTipoliquidacion>) qry.list();
+        /* } catch (Exception ex) {
+         ex.printStackTrace();
+         } finally {
+         sesion.close();
+         }*/
         return listaTLiq;
     }
-    
-    
 
+    //---------Lista pesaje que todavia no fueron cobrados por el cliente---------------------//
+    public List listarPesajePendiente(String rucemp, Date fechaInicio, Date fechaFin) {
+        List<TPesaje> listaTLiq = null;
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm");
+        try {
+            //String fechaI=sdf.format(fechaInicio);
+            //String fechaF=sdf.format(fechaFin);
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            qry = sesion.createQuery("SELECT p FROM TPesaje p inner join p.TControlviaje cv where p.estadoPesaje='P' and cv.TDespachodet.id.cdRuc='" + rucemp + "' ");//and p.fechaPesaje between '"+fechaI+"' and '"+fechaF+"'");            
+            listaTLiq = (List<TPesaje>) qry.list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            ex.getMessage();
+        } /*finally {
+         sesion.close();
+         }*/
+        return listaTLiq;
+    }
+
+    public boolean IngresarLiquidacion(TLiquidacion liquidacion, TPesaje[] selectedPesaje) {
+        FacesContext context1 = FacesContext.getCurrentInstance();
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            trans = sesion.beginTransaction();
+            String idcl = obtenerIDLiquidacion();
+            if (idcl.equals("")) {
+                //context1.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al generar NUMERACION","");
+                return false;
+            }
+            liquidacion.setCdLiq(idcl);
+            sesion.save(liquidacion);
+            actualizarPesajexLiquidacion(liquidacion, selectedPesaje);
+            ingresarEstadoxLiquidacionInicial(liquidacion, false);
+            //if (!trans.wasCommitted()) {
+            trans.commit();
+            //}
+        } catch (Exception ex) {
+            //despues agrego para que salgan mensajes de error            
+            context1.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al generar Liquidación", ex.getMessage()));
+            trans.rollback();
+            //ex.printStackTrace();
+            return false;
+        } finally {
+            sesion.close();
+        }
+        return true;
+    }
+
+    private String obtenerIDLiquidacion() {
+        String nuevoID = "";
+        try {
+            qry = sesion.createSQLQuery("select idLiquidacion2();");
+            ArrayList<String> idNuevo = (ArrayList<String>) qry.list();
+            System.out.println("llamando a funcion");
+            System.out.println(idNuevo);
+            nuevoID = idNuevo.size() > 0 ? idNuevo.get(0) : "";
+        } catch (Exception ex) {
+            nuevoID = "";
+        }
+        return nuevoID;
+    }
+
+    /*METODO QUE AL GENERA LIQUIDACION EMPIEZA CON EL ESTADO INICIAL - PENDIENTE*/
+    public void ingresarEstadoxLiquidacionInicial(TLiquidacion liquidacion, boolean ver) {
+        try {
+            TEstadoliq obj = new TEstadoliq();
+            obj.setCdEstadoLiq("EL1");
+            TEstadoxliquidacionId estadoLiq = new TEstadoxliquidacionId();
+            estadoLiq.setCdLiq(liquidacion.getCdLiq());
+            estadoLiq.setCdEstadoLiq("EL1");
+            estadoLiq.setFechaEstado(liquidacion.getFechaApertura());
+            TEstadoxliquidacion nuevo = new TEstadoxliquidacion();
+            nuevo.setId(estadoLiq);
+            nuevo.setObs("Liquidación Pendiente para ser cobrada");
+            nuevo.setTEstadoliq(obj);
+            nuevo.setTLiquidacion(liquidacion);
+            sesion.save(nuevo);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+
+    /*METODO QUE ACTUALIZA LOS PESAJES ASOCIADOS A LA LIQUIDACION GENERADA*/
+    public void actualizarPesajexLiquidacion(TLiquidacion liquidacion, TPesaje[] pesaje) {
+        FacesContext context1 = FacesContext.getCurrentInstance();
+        try {
+            for (TPesaje tp : pesaje) {
+                TPesaje dtopesaje = new TPesaje();
+                dtopesaje = tp;
+                dtopesaje.setTLiquidacion(liquidacion);
+                sesion.merge(dtopesaje);
+                //sesion.beginTransaction().commit(); no volver a hacer esto xD
+            }
+        } catch (Exception e) {
+            sesion.beginTransaction().rollback();
+            context1.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Actualiza Pesaje Asociados de Liquidacion", e.getMessage()));
+        }
+    }
 }
