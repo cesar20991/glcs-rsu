18e727ba2bdd7ff781d61f25ca17d6595ede90e4
cesar20991 <cesar20991@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-06-03 19:45:14 +0000

    Se realizo un porcentaje del CUS Adm. Comprobante

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/PesajeBean.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/PesajeBean.java
@@ -1,29 +1,40 @@
 package emch.controller.bean;
 
-
 import emch.modelo.acceso.*;
 import emch.modelo.entidades.*;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 @ManagedBean
 @SessionScoped
 public class PesajeBean extends UsuarioBean {
+
     private TLiquidacion selectedLiquidacion;
     private List<TPesaje> listarPesaje;
     private List<TComprobante> listarComprobante;
-    
     //-----------GenerarPesaje---------------------------
     private TPesaje pesaje;
-    private List<TPesaje> generapesaje;
+    private DataModel listpesajes;
+    private boolean esEdicion;
+    private String accion;
+    private List<TControlviaje> listacontrolviaje;
+    private TControlviaje selectedcv;
+    //private List<TPesaje> listpesajes;
     //---------------------------------------------------
-    
-    @PostConstruct
+
+    @Override
     public void init() {
-        
+        pesaje = new TPesaje();
+        pesaje.setTControlviaje(new TControlviaje());
+        pesaje.setTLiquidacion(new TLiquidacion());
+        pesaje.setTControlviaje(new TControlviaje(new TControlviajeId(), null, null, "", ""));
     }
 
     public TLiquidacion getSelectedLiquidacion() {
@@ -42,8 +53,8 @@ public class PesajeBean extends UsuarioBean {
 
     public void setListarPesaje(List<TPesaje> listarPesaje) {
         this.listarPesaje = listarPesaje;
-    }   
-    
+    }
+
     public List<TComprobante> getListarComprobante() {
         ComprobanteManaged objCom = new ComprobanteManaged();
         listarComprobante = objCom.listarComprobante(selectedLiquidacion);
@@ -53,5 +64,89 @@ public class PesajeBean extends UsuarioBean {
     public void setListarComprobante(List<TComprobante> listarComprobante) {
         this.listarComprobante = listarComprobante;
     }
-       
+
+    //----------------COMPROBANTE PESAJE-----------------------//
+    public TPesaje getPesaje() {
+        return pesaje;
+    }
+
+    public void setPesaje(TPesaje pesaje) {
+        this.pesaje = pesaje;
+    }
+
+    public DataModel getListpesajes() {
+        PesajeManaged obj = new PesajeManaged();
+        setListpesajes(new ListDataModel(obj.listarPesajes()));
+        return listpesajes;
+    }
+
+    public void setListpesajes(DataModel listpesajes) {
+        this.listpesajes = listpesajes;
+    }
+
+    public String irAgregar() {
+        setEsEdicion(false);
+        setPesaje(new TPesaje());
+        pesaje.setTControlviaje(new TControlviaje());
+        pesaje.setTLiquidacion(new TLiquidacion());
+        return "nuevopesaje";
+    }
+    
+    public String irActualizar() {
+        setEsEdicion(true);
+        setPesaje((TPesaje) listpesajes.getRowData());
+        String controlViaje = pesaje.getTControlviaje().getId().getCdControlViaje();
+        int nroViaje = pesaje.getTControlviaje().getId().getNroViaje();
+        pesaje.setTControlviaje(new TControlviaje(new TControlviajeId(controlViaje, nroViaje), null, null, "", ""));
+//        //String cdliq = pesaje.getTLiquidacion().getCdLiq();
+//        //pesaje.setTLiquidacion(new TLiquidacion(cdliq, null, null, null, null, null, "", null));
+        String cod = pesaje.getCdPesaje();
+        pesaje.setCdPesaje(cod);
+        return "nuevopesaje";
+    }
+
+    public boolean isEsEdicion() {
+        return esEdicion;
+    }
+
+    public void setEsEdicion(boolean esEdicion) {
+        this.esEdicion = esEdicion;
+    }
+
+    public String getAccion() {
+        return isEsEdicion() ? "Actualizar Pesaje" : "Agregar Pesaje";
+    }
+
+    public String ingresar() {
+        FacesContext context1 = FacesContext.getCurrentInstance();
+        PesajeManaged pesajeMgd = new PesajeManaged();
+        //String a = selectedcontrolviaje.getId().getCdControlViaje();
+        boolean resultado = isEsEdicion() ? pesajeMgd.actualizarPesaje(pesaje,selectedcv,"coropeza") : pesajeMgd.ingresarPesaje(pesaje,selectedcv,"coropeza");
+        if (resultado) {
+            context1.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Se agrego el Pesaje " + pesaje.getNroPesaje() + " Correctamente", "Verificar"));
+            return "pesajes";
+        } else {
+            context1.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Crear Pesaje", "Verificar"));
+            return ""; //futuros errores
+        }
+    }
+    
+    public List<TControlviaje> getListacontrolviaje() {
+        PesajeManaged obj = new PesajeManaged();
+        listacontrolviaje = obj.listarControlViaje();
+        return listacontrolviaje;
+    }
+
+    public void setListacontrolviaje(List<TControlviaje> listacontrolviaje) {
+        this.listacontrolviaje = listacontrolviaje;
+    }
+
+    public TControlviaje getSelectedcv() {
+        return selectedcv;
+    }
+
+    public void setSelectedcv(TControlviaje selectedcv) {
+        this.selectedcv = selectedcv;
+    }
+    
 }
