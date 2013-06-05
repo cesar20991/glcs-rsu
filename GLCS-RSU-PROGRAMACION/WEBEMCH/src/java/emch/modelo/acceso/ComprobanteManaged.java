47d68f65683cde14f45453b745227f7f5f256797
vic.nun.les@gmail.com <vic.nun.les@gmail.com@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-06-05 10:33:11 +0000

    comprobante managed actualizado con metodos para listar

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/ComprobanteManaged.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/ComprobanteManaged.java
@@ -16,31 +16,61 @@ public class ComprobanteManaged {
 
     public List listarComprobante(TLiquidacion selectedLiquidacion) {
         List<TComprobante> listaCom = null;
-      /*  try {*/
-            sesion = HibernateUtil.getSessionFactory().openSession();
-            trans = sesion.beginTransaction();
-            qry = sesion.createQuery("SELECT a FROM TComprobante a where a.TLiquidacion.cdLiq='"+selectedLiquidacion.getCdLiq()+"'");
-            listaCom = (List<TComprobante>) qry.list();
-       /* } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            sesion.close();
-        }*/
+        /*  try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        trans = sesion.beginTransaction();
+        qry = sesion.createQuery("SELECT a FROM TComprobante a where a.TLiquidacion.cdLiq='" + selectedLiquidacion.getCdLiq() + "'");
+        listaCom = (List<TComprobante>) qry.list();
+        /* } catch (Exception ex) {
+         ex.printStackTrace();
+         } finally {
+         sesion.close();
+         }*/
+        return listaCom;
+    }
+
+    public List listarComprobantes() {
+        List<TComprobante> listaCom = null;
+        /*  try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        trans = sesion.beginTransaction();
+        qry = sesion.createQuery("FROM TComprobante");
+        listaCom = (List<TComprobante>) qry.list();
+        /* } catch (Exception ex) {
+         ex.printStackTrace();
+         } finally {
+         sesion.close();
+         }*/
         return listaCom;
     }
     
-        public List listarServicio() {
+        public List listarComprobantesDets(TComprobante comp) {
+        List<TComprobante> listaCom = null;
+        /*  try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        trans = sesion.beginTransaction();
+        qry = sesion.createQuery("FROM TComprobantedet where idComprobante='"+comp.getIdComprobante()+"'");
+        listaCom = (List<TComprobante>) qry.list();
+        /* } catch (Exception ex) {
+         ex.printStackTrace();
+         } finally {
+         sesion.close();
+         }*/
+        return listaCom;
+    }
+
+    public List listarServicio() {
         List<TServicio> listatipo = null;
-      /*  try {*/
-            sesion = HibernateUtil.getSessionFactory().openSession();
-            trans = sesion.beginTransaction();
-            qry = sesion.createQuery("FROM TServicio");
-            listatipo = (List<TServicio>) qry.list();
-       /* } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            sesion.close();
-        }*/
+        /*  try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        trans = sesion.beginTransaction();
+        qry = sesion.createQuery("FROM TServicio");
+        listatipo = (List<TServicio>) qry.list();
+        /* } catch (Exception ex) {
+         ex.printStackTrace();
+         } finally {
+         sesion.close();
+         }*/
         return listatipo;
     }
 
