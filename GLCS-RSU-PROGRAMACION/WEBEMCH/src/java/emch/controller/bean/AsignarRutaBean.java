6bc2603d79d7bae2f38b36089bd5a1adae5de31e
cesar20991 <cesar20991@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-06-06 18:22:35 +0000

    70% del CUS ASIGNAR RUTA

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/AsignarRutaBean.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/AsignarRutaBean.java
@@ -2,10 +2,17 @@ package emch.controller.bean;
 
 import emch.modelo.acceso.MapsManaged;
 import emch.modelo.entidades.TAsignarruta;
+import emch.modelo.entidades.TDespacho;
 import emch.modelo.entidades.TDespachodet;
+import emch.modelo.entidades.TDespachodetId;
+import emch.modelo.entidades.TEmpresa;
+import emch.modelo.entidades.TTrabajadorxcamion;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -13,17 +20,18 @@ import javax.servlet.http.HttpServletRequest;
  * @author Administrador
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class AsignarRutaBean {
 
     private TDespachodet selecteddespachodet;
     private TAsignarruta asignaruta;
     private List<TDespachodet> listaDespachoDet;
+    private List<TEmpresa> listarempresa;
 
     public AsignarRutaBean() {
         asignaruta = new TAsignarruta();
         asignaruta.setTDespachodet(new TDespachodet());
-        //selecteddespachodet.setId(new TDespachodetId());
+        asignaruta.setTDespachodet(new TDespachodet(new TDespachodetId(1, null, null), null, null, 1));
     }
 
     public TDespachodet getSelecteddespachodet() {
@@ -35,12 +43,24 @@ public class AsignarRutaBean {
     }
 
     public String irAsignarRuta() {
+        
         return "asignarruta";
     }
 
     public String irInsertar() {
-        
-        return "";
+        FacesContext context = FacesContext.getCurrentInstance();
+        //asignaruta.setTDespachodet(selecteddespachodet);
+        MapsManaged mgd = new MapsManaged();
+        boolean resultado = mgd.ingresarRuta(asignaruta,selecteddespachodet);
+         if (resultado) {
+            setAsignaruta(null);
+            context.addMessage(null, new FacesMessage("Ase asigno ruta correctamente", "Verificar"));
+            return "listardespacho";
+        } else {
+            //setEsEdicion(false);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Asignar ruta", "Verificar"));
+            return "asignarruta"; //futuros errores
+        }
     }
 
     /**
@@ -71,6 +91,22 @@ public class AsignarRutaBean {
      */
     public void setListaDespachoDet(List<TDespachodet> listaDespachoDet) {
         this.listaDespachoDet = listaDespachoDet;
+    }
+
+    /**
+     * @return the listarempresa
+     */
+    public List<TEmpresa> getListarempresa() {
+        MapsManaged obj = new MapsManaged();
+        listarempresa = obj.listarEmpresa();
+        return listarempresa;
+    }
+
+    /**
+     * @param listarempresa the listarempresa to set
+     */
+    public void setListarempresa(List<TEmpresa> listarempresa) {
+        this.listarempresa = listarempresa;
     }
     
 }
--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/MapsManaged.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/MapsManaged.java
@@ -6,8 +6,13 @@ package emch.modelo.acceso;
 
 import emch.modelo.entidades.TAsignarruta;
 import emch.modelo.entidades.TDespachodet;
+import emch.modelo.entidades.TDespachodetId;
+import emch.modelo.entidades.TEmpresa;
 import emch.modelo.entidades.TLiquidacion;
+import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import org.hibernate.Query;
 import org.hibernate.Transaction;
 import org.hibernate.classic.Session;
@@ -36,17 +41,68 @@ public class MapsManaged {
          }*/
         return listaLiq;
     }
-    
+
+    public List listarEmpresa() {
+        List<TEmpresa> listaLiq = null;
+        /*  try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        qry = sesion.createQuery("FROM TEmpresa");
+        listaLiq = (List<TEmpresa>) qry.list();
+        /* } catch (Exception ex) {
+         ex.printStackTrace();
+         } finally {
+         sesion.close();
+         }*/
+        return listaLiq;
+    }
+
     public List ListarDespachoxDespachoDet() {
         List<TDespachodet> listadespachoDet = null;
         try {
-            sesion = HibernateUtil.getSessionFactory().openSession();            
-            qry = sesion.createQuery( "SELECT ab from TDespachodet ab order by ab.TDespacho.nroDespacho");//  v inner join v.TDespachodets ab where v.id.cdDespacho ='"+ despacho+"'");
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            qry = sesion.createQuery("SELECT ab from TDespachodet ab order by ab.TDespacho.nroDespacho");//  v inner join v.TDespachodets ab where v.id.cdDespacho ='"+ despacho+"'");
             listadespachoDet = (List<TDespachodet>) qry.list();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         //finally{sesion.close();}
         return listadespachoDet;
+    }
+
+    public boolean ingresarRuta(TAsignarruta asignarruta, TDespachodet selecteddespachodet) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            trans = sesion.beginTransaction();
+            String idcl = obtenerIDAsignarruta();
+            if (idcl.equals("")) {
+                return false;
+            }
+            asignarruta.setTDespachodet(selecteddespachodet);
+            asignarruta.setCdRuta(idcl);
+            sesion.save(asignarruta);
+            trans.commit();
+        } catch (Exception ex) {
+            context.addMessage(null, new FacesMessage("Error al asignar ruta", "Verificar"));
+            //despues agrego para que salgan mensajes de error            
+            trans.rollback();
+            ex.printStackTrace();
+            return false;
+        } finally {
+            sesion.close();
+        }
+        return true;
+    }
+
+    private String obtenerIDAsignarruta() {
+        String nuevoID = "";
+        /* try {*/
+        qry = sesion.createSQLQuery("select idAsignaruta();");
+        ArrayList<String> idNuevo = (ArrayList<String>) qry.list();
+        nuevoID = idNuevo.size() > 0 ? idNuevo.get(0) : "";
+        /* } catch (Exception ex) {
+         nuevoID = "";
+         }*/
+        return nuevoID;
     }
 }
