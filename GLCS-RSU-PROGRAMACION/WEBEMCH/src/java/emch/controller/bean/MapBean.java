5362fefcf2674e491d991b0e3fe63793208a3824
vic.nun.les@gmail.com <vic.nun.les@gmail.com@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-06-13 23:12:03 +0000

    sace metodos de aqui a asignar ruta bean

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/MapBean.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/MapBean.java
@@ -6,6 +6,7 @@ package emch.controller.bean;
 
 import emch.modelo.acceso.MapsManaged;
 import emch.modelo.entidades.TAsignarruta;
+import emch.modelo.entidades.TDespachodet;
 import emch.modelo.entidades.TTipotrabajador;
 import emch.modelo.entidades.TTrabajador;
 import emch.modelo.entidades.TUbigeo;
@@ -16,6 +17,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
@@ -27,16 +29,17 @@ import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Rectangle;
 
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class MapBean implements Serializable {
 
     private MapModel rectangleModel;
     private List<TAsignarruta> listasignarruta;
+    AsignarRutaBean selectedDesDet = new AsignarRutaBean();
 
     
     @PostConstruct
     public void init() {
-        getListasignarruta();
+        //getListasignarruta();
     }
     
     public MapBean() {
@@ -78,33 +81,5 @@ public class MapBean implements Serializable {
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
-    public List<TAsignarruta> getListasignarruta() {
-        MapsManaged obj = new MapsManaged();
-        listasignarruta = obj.listarRutaAsignadaTodos();
-        GenerarMapsTodos();
-        return listasignarruta;
-    }
-
-    public void setListasignarruta(List<TAsignarruta> listasignarruta) {
-        this.listasignarruta = listasignarruta;
-    }
-
-    private void GenerarMapsTodos() {
-        rectangleModel = new DefaultMapModel();
-        LatLng coord1;
-        LatLng coord2;
-        Rectangle rect;
-        //Shared coordinates  
-        for (int i = 0; i < listasignarruta.size(); i++) {
-            coord1 = new LatLng(listasignarruta.get(i).getLatn(), listasignarruta.get(i).getLngn());
-            coord2 = new LatLng(listasignarruta.get(i).getLats(), listasignarruta.get(i).getLngs());
-            //Rectangle  
-            rect = new Rectangle(new LatLngBounds(coord1, coord2));
-            rect.setStrokeColor("#d93c3c");
-            rect.setFillColor("#d93c3c");
-            rect.setFillOpacity(0.5);
-
-            rectangleModel.addOverlay(rect);
-        }
-    }
+    
 }
