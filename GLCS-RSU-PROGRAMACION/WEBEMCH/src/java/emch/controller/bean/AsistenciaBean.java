4f703e69bbfbe7020fa5f22bc99381551c731d82
mariorobles1693@gmail.com <mariorobles1693@gmail.com@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-06-15 22:36:20 +0000

    Se Modifico partes del Cus Asistencia en la programación.

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/AsistenciaBean.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/AsistenciaBean.java
@@ -24,26 +24,29 @@ public class AsistenciaBean {
     private String sfechaActual = fechaActual.getDate() + " / " + (fechaActual.getMonth() + 1) + " / " + (fechaActual.getYear() - 100);
     private String cdAsistencia = "";
     private List<TTrabajador> listatrabajador;
+    private List<TTrabajador> listatrabajador2;
+    private List<TAsistencia> listaAsistencia;
     private TTrabajador trabajador;
     private TAsistencia asistencia;
+    private TAsistencia asistenciaCuadro;
     private TTurno turno;
-    private int opcion = 0;
 
     public AsistenciaBean() {
         trabajador = new TTrabajador();
-        if(fechaActual.getHours()>=8 & fechaActual.getHours()<20){
-            cdAsistencia += "M"+fechaActual.getDate();
+        if (fechaActual.getHours() >= 8 & fechaActual.getHours() < 20) {
+            cdAsistencia += "M" + fechaActual.getDate();
             turno = new TTurno("TN01", "Mañana");
-        }else if((fechaActual.getHours()>=20 || fechaActual.getHours()<8)){
-            cdAsistencia += "N"+(fechaActual.getDate()+1);
+        } else if ((fechaActual.getHours() >= 20 || fechaActual.getHours() < 8)) {
+            cdAsistencia += "N" + (fechaActual.getDate() + 1);
             turno = new TTurno("TN02", "Noche");
         }
         AsistenciaManaged obj = new AsistenciaManaged();
         TAsistencia ta = new TAsistencia(cdAsistencia, turno, fechaActual, "mar1693", fechaActual);
-        if(!obj.ExisteIdAsistencia(cdAsistencia)){            
+        if (!obj.ExisteIdAsistencia(cdAsistencia)) {
             obj.insertarAsistencia(ta);
-        }        
-    }    
+        }
+        asistenciaCuadro = new TAsistencia(cdAsistencia, turno, fechaActual, "mar1693", fechaActual);
+    }
 
     public Date getFechajust() {
         return fechajust;
@@ -71,9 +74,9 @@ public class AsistenciaBean {
 
     public List<TTrabajador> getListatrabajador() {
         AsistenciaManaged obj = new AsistenciaManaged();
-        if(!obj.ExisteIdAsistencia(cdAsistencia)){
+        if (!obj.ExisteIdAsistencia(cdAsistencia)) {
             listatrabajador = obj.todosTrabajador();
-        }else{
+        } else {
             listatrabajador = obj.todosTrabajadoresSinAsistencia(cdAsistencia);
         }
         return listatrabajador;
@@ -81,6 +84,16 @@ public class AsistenciaBean {
 
     public void setListatrabajador(List<TTrabajador> listatrabajador) {
         this.listatrabajador = listatrabajador;
+    }
+
+    public List<TTrabajador> getListatrabajador2() {
+        AsistenciaManaged obj = new AsistenciaManaged();
+        listatrabajador2 = obj.todosTrabajador();
+        return listatrabajador2;
+    }
+
+    public void setListatrabajador2(List<TTrabajador> listatrabajador2) {
+        this.listatrabajador2 = listatrabajador2;
     }
 
     public TTrabajador getTrabajador() {
@@ -91,33 +104,68 @@ public class AsistenciaBean {
         this.trabajador = trabajador;
     }
 
-    public int getOpcion() {
-        return opcion;
-    }
-
-    public void setOpcion(int opcion) {
-        this.opcion = opcion;
-    }    
-
     public String getCdAsistencia() {
         return cdAsistencia;
     }
 
     public void setCdAsistencia(String cdAsistencia) {
         this.cdAsistencia = cdAsistencia;
-    }   
-    
-    public void MarcarAsistencia(TTrabajador trabajador){        
+    }
+
+    public void MarcarAsistencia(TTrabajador trabajador) {
         AsistenciaManaged obj = new AsistenciaManaged();
         TAsistenciaxtrabajadorId asistenciaxtrabajadorId = new TAsistenciaxtrabajadorId(trabajador.getCdTrabajador(), cdAsistencia);
         TAsistenciaxtrabajador asistenciaxtrabajador = new TAsistenciaxtrabajador(asistenciaxtrabajadorId, trabajador, asistencia, true, false, false);
         obj.insertarAsistenciaxTrabajador(asistenciaxtrabajador);
     }
-    
-    public void MarcarTardanza(TTrabajador trabajador){        
+
+    public void MarcarTardanza(TTrabajador trabajador) {
         AsistenciaManaged obj = new AsistenciaManaged();
         TAsistenciaxtrabajadorId asistenciaxtrabajadorId = new TAsistenciaxtrabajadorId(trabajador.getCdTrabajador(), cdAsistencia);
         TAsistenciaxtrabajador asistenciaxtrabajador = new TAsistenciaxtrabajador(asistenciaxtrabajadorId, trabajador, asistencia, false, true, false);
         obj.insertarAsistenciaxTrabajador(asistenciaxtrabajador);
+    }
+
+    public List<TAsistencia> getListaAsistencia() {
+        AsistenciaManaged obj = new AsistenciaManaged();
+        listaAsistencia = obj.todasAsistencias();
+        return listaAsistencia;
+    }
+
+    public void setListaAsistencia(List<TAsistencia> listaAsistencia) {
+        this.listaAsistencia = listaAsistencia;
+    }
+
+    public TAsistencia getAsistencia() {
+        return asistencia;
+    }
+
+    public void setAsistencia(TAsistencia asistencia) {
+        this.asistencia = asistencia;
+    }
+
+    public TAsistencia getAsistenciaCuadro() {
+        return asistenciaCuadro;
+    }
+
+    public void setAsistenciaCuadro(TAsistencia asistenciaCuadro) {
+        this.asistenciaCuadro = asistenciaCuadro;
+    }
+
+    public String EstadoAsistenciaxTrabajador(TTrabajador tTrabajador) {
+        AsistenciaManaged obj = new AsistenciaManaged();
+        TAsistenciaxtrabajador taxt = obj.AsistenciaxTrabajador(asistenciaCuadro, tTrabajador);
+        if (taxt == null) {
+            return "Falto";
+        } else if (taxt.getAsiste()) {
+            return "Asistio";
+        } else if (taxt.getTardanza()) {
+            return "Tardanza";
+        }
+        return "";
+    }
+
+    public void PreperarASistenciaCuadro(TAsistencia ta) {
+        asistenciaCuadro = ta;
     }
 }
