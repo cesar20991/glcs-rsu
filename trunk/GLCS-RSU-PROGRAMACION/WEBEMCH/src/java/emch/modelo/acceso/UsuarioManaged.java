2d54b677849d19244bbed7325f83f9f42a31cfe3
vic.nun.les@gmail.com <vic.nun.les@gmail.com@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-06-18 08:43:46 +0000

    agregando el login del cliente y los templates

--- trunk/GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/UsuarioManaged.java
+++ trunk/GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/UsuarioManaged.java
@@ -1,6 +1,7 @@
 
 package emch.modelo.acceso;
 
+import emch.modelo.entidades.TCliente;
 import emch.modelo.entidades.TEmpresa;
 import emch.modelo.entidades.TUsuario;
 
@@ -9,6 +10,6 @@ public interface UsuarioManaged {
     public TUsuario buscarPorUsuario(TUsuario usuario);
     public TEmpresa ListarEmpresa();
     public TEmpresa BuscarPorEmpresa(TEmpresa empresa);
-    
+    public TEmpresa buscarRuc(TUsuario usuario); 
     
 }
--- trunk/GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/UsuarioManagedImpl.java
+++ trunk/GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/UsuarioManagedImpl.java
@@ -1,28 +1,37 @@
-
 package emch.modelo.acceso;
 
+import emch.modelo.entidades.TCliente;
 import emch.modelo.entidades.TEmpresa;
 import emch.modelo.entidades.TUsuario;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import util.HibernateUtil;
 
-public class UsuarioManagedImpl implements UsuarioManaged{
+public class UsuarioManagedImpl implements UsuarioManaged {
 
     @Override
     public TUsuario buscarPorUsuario(TUsuario usuario) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        String sql ="SELECT u FROM TUsuario u where u.nomUsu=:user and u.pass=:pass";
+        String sql = "SELECT u FROM TUsuario u where u.nomUsu=:user and u.pass=:pass";
         Query query = session.createQuery(sql);
         query.setString("user", usuario.getNomUsu());
         query.setString("pass", usuario.getPass());
         return (TUsuario) query.uniqueResult();
     }
+    
+   @Override
+   public TEmpresa buscarRuc(TUsuario usuario) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        String sql = "SELECT ab FROM TCliente c  inner join c.TEmpresa ab where c.TUsuario.nomUsu=:nonUsu";
+        Query query = session.createQuery(sql);
+        query.setString("nonUsu", usuario.getNomUsu());
+        return (TEmpresa) query.uniqueResult();
+    }
 
     @Override
     public TEmpresa ListarEmpresa() {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        String sql ="FROM TEmpresa";
+        String sql = "FROM TEmpresa";
         Query query = session.createQuery(sql);
         return (TEmpresa) query.list();
     }
@@ -30,10 +39,9 @@ public class UsuarioManagedImpl implements UsuarioManaged{
     @Override
     public TEmpresa BuscarPorEmpresa(TEmpresa empresa) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        String sql ="SELECT u FROM TEmpresa u where u.cdRuc=:cdruc";
+        String sql = "SELECT u FROM TEmpresa u where u.cdRuc=:cdruc";
         Query query = session.createQuery(sql);
         query.setString("cdruc", empresa.getCdRuc());
         return (TEmpresa) query.uniqueResult();
     }
-    
 }
