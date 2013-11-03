704e93392b717ab58ca0eae29b3749185589b137
cesar20991 <cesar20991@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-11-03 20:55:35 +0000

    se agrego para enviar un id a otra vista

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/HojadeDespachoManaged.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/modelo/acceso/HojadeDespachoManaged.java
@@ -14,28 +14,27 @@ import org.hibernate.Transaction;
 
 public class HojadeDespachoManaged {
 
-   Session sesion;
+    Session sesion;
     Transaction trans;
     Query qry;
 
-    
     public List<TDespacho> buscarTodos() {
-            List<TDespacho> listadespacho = null;
-      /*  try {*/
-            sesion = HibernateUtil.getSessionFactory().openSession();
-            trans = sesion.beginTransaction();
-            qry = sesion.createQuery("FROM TDespacho");
-            listadespacho = (List<TDespacho>) qry.list();
-            return listadespacho;
+        List<TDespacho> listadespacho = null;
+        /*  try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        trans = sesion.beginTransaction();
+        qry = sesion.createQuery("FROM TDespacho");
+        listadespacho = (List<TDespacho>) qry.list();
+        return listadespacho;
     }
 
     public TDespacho buscarPorId(String id) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         return (TDespacho) session.load(TDespacho.class, id);
     }
-    
+
     public boolean actualizar(TDespacho despacho) {
-         sesion = HibernateUtil.getSessionFactory().openSession();
+        sesion = HibernateUtil.getSessionFactory().openSession();
         try {
             sesion.beginTransaction();
             sesion.merge(despacho);
@@ -47,8 +46,8 @@ public class HojadeDespachoManaged {
         return true;
     }
 
-    public boolean insertar(TDespacho despacho, List<TTrabajadorxcamion> trabajadorxcamion ) {
-         sesion = HibernateUtil.getSessionFactory().openSession();
+    public boolean insertar(TDespacho despacho, List<TTrabajadorxcamion> trabajadorxcamion) {
+        sesion = HibernateUtil.getSessionFactory().openSession();
         try {
             sesion.beginTransaction();
             trans = sesion.beginTransaction();
@@ -57,36 +56,36 @@ public class HojadeDespachoManaged {
                 return false;
             }
             Date hoy = new Date();
-             TDespachoId despID = new TDespachoId();
-             despID.setCdDespacho(idcl);
-             despID.setCdRuc("RC0001");
-             despacho.setUsuCrea("coropeza");
-             despacho.setFechaReg(despacho.getFechaEmi());
-             despacho.setId(despID);
+            TDespachoId despID = new TDespachoId();
+            despID.setCdDespacho(idcl);
+            despID.setCdRuc("RC0001");
+            despacho.setUsuCrea("coropeza");
+            despacho.setFechaReg(despacho.getFechaEmi());
+            despacho.setId(despID);
             sesion.save(despacho);
-            ingresarDespachoDet(despacho,trabajadorxcamion);
+            ingresarDespachoDet(despacho, trabajadorxcamion);
             trans.commit();
         } catch (Exception e) {
-            System.out.println("Error en insertar" + e.getMessage());           
+            System.out.println("Error en insertar" + e.getMessage());
             trans.rollback();
             return false;
         }
         return true;
     }
-    
+
     public void ingresarDespachoDet(TDespacho despacho, List<TTrabajadorxcamion> trabajadorxcamion) {
         try {
-            int a=1;
-             for (TTrabajadorxcamion tp : trabajadorxcamion) {                 
-                 TDespachodetId id = new TDespachodetId();
-                 id.setCdDespacho(despacho.getId().getCdDespacho());
-                 id.setNro(a);
-                 id.setCdRuc("RC0001");
-                 TDespachodet dtodet = new TDespachodet();
+            int a = 1;
+            for (TTrabajadorxcamion tp : trabajadorxcamion) {
+                TDespachodetId id = new TDespachodetId();
+                id.setCdDespacho(despacho.getId().getCdDespacho());
+                id.setNro(a);
+                id.setCdRuc("RC0001");
+                TDespachodet dtodet = new TDespachodet();
                 dtodet.setId(id);
                 dtodet.setCantViaje(2);
                 dtodet.setTDespacho(despacho);
-                dtodet.setTTrabajadorxcamion(tp);                
+                dtodet.setTTrabajadorxcamion(tp);
                 sesion.save(dtodet);
                 a++;
             }
@@ -94,53 +93,54 @@ public class HojadeDespachoManaged {
             System.out.print(e.getMessage());
         }
     }
-    
+
     public List listarUbigeo() {
         List<TUbigeo> listaubigeo = null;
-       /* try {*/
-            sesion = HibernateUtil.getSessionFactory().openSession();
-            trans = sesion.beginTransaction();
-            qry = sesion.createQuery("FROM TUbigeo");
-            listaubigeo = (List<TUbigeo>) qry.list();
-       /* } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            sesion.close();
-       }*/
+        /* try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        trans = sesion.beginTransaction();
+        qry = sesion.createQuery("FROM TUbigeo");
+        listaubigeo = (List<TUbigeo>) qry.list();
+        /* } catch (Exception ex) {
+         ex.printStackTrace();
+         } finally {
+         sesion.close();
+         }*/
         return listaubigeo;
     }
+
     public List listarturno() {
         List<TTurno> listaturno = null;
-       /* try {*/
-            sesion = HibernateUtil.getSessionFactory().openSession();
-            trans = sesion.beginTransaction();
-            qry = sesion.createQuery("FROM TTurno");
-            listaturno = (List<TTurno>) qry.list();
-       /* } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            sesion.close();
-       }*/
+        /* try {*/
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        trans = sesion.beginTransaction();
+        qry = sesion.createQuery("FROM TTurno");
+        listaturno = (List<TTurno>) qry.list();
+        /* } catch (Exception ex) {
+         ex.printStackTrace();
+         } finally {
+         sesion.close();
+         }*/
         return listaturno;
     }
-    
+
     private String obtenerIDDespacho() {
         String nuevoID = "";
-       /* try {*/
-            qry = sesion.createSQLQuery("select idDespacho();");
-            ArrayList<String> idNuevo = (ArrayList<String>) qry.list();
-            nuevoID = idNuevo.size() > 0 ? idNuevo.get(0) : "";
-      /* } catch (Exception ex) {
-            nuevoID = "";
-        }*/
+        /* try {*/
+        qry = sesion.createSQLQuery("select idDespacho();");
+        ArrayList<String> idNuevo = (ArrayList<String>) qry.list();
+        nuevoID = idNuevo.size() > 0 ? idNuevo.get(0) : "";
+        /* } catch (Exception ex) {
+         nuevoID = "";
+         }*/
         return nuevoID;
     }
-    
+
     public List ListarDespachoxDespachoDet(TDespacho despacho) {
         List<TDespachodet> listadespachoDet = null;
         try {
-            sesion = HibernateUtil.getSessionFactory().openSession();            
-            qry = sesion.createQuery( "SELECT ab from TDespacho  v inner join v.TDespachodets ab where v.id.cdDespacho ='"+ despacho.getId().getCdDespacho()+"'");
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            qry = sesion.createQuery("SELECT ab from TDespacho  v inner join v.TDespachodets ab where v.id.cdDespacho ='" + despacho.getId().getCdDespacho() + "'");
             listadespachoDet = (List<TDespachodet>) qry.list();
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -148,12 +148,12 @@ public class HojadeDespachoManaged {
         //finally{sesion.close();}
         return listadespachoDet;
     }
-    
+
     public List ListarDespachoxDespachoDet(String despacho) {
         List<TDespachodet> listadespachoDet = null;
         try {
-            sesion = HibernateUtil.getSessionFactory().openSession();            
-            qry = sesion.createQuery( "SELECT ab from TDespacho  v inner join v.TDespachodets ab where v.id.cdDespacho ='"+ despacho+"'");
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            qry = sesion.createQuery("SELECT ab from TDespacho  v inner join v.TDespachodets ab where v.id.cdDespacho ='" + despacho + "'");
             listadespachoDet = (List<TDespachodet>) qry.list();
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -161,17 +161,17 @@ public class HojadeDespachoManaged {
         //finally{sesion.close();}
         return listadespachoDet;
     }
-    
-     public List buscaTrabxCamion(String despacho) {
-         List<TTrabajadorxcamion> listatrabxc = null;
-         try {
+
+    public List buscaTrabxCamion(String despacho) {
+        List<TTrabajadorxcamion> listatrabxc = null;
+        try {
             sesion = HibernateUtil.getSessionFactory().openSession();
-           qry = sesion.createQuery( "SELECT ab  from TDespachodet v inner join v.TTrabajadorxcamion  ab where v.id.cdDespacho='"+ despacho+"'");
+            qry = sesion.createQuery("SELECT ab  from TDespachodet v inner join v.TTrabajadorxcamion  ab where v.id.cdDespacho='" + despacho + "'");
             listatrabxc = (List<TTrabajadorxcamion>) qry.list();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return listatrabxc;
-     }
-    
+    }
+
 }
