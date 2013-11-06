7cbc216eb7d591a6ac45e55cd44c5c4445f15bcc
gechy9375@gmail.com <gechy9375@gmail.com@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-11-06 06:08:55 +0000

    Modifiqué linea de autor

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/verificarcomprobanteBean.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/verificarcomprobanteBean.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package emch.controller.bean;
 
 import emch.modelo.acceso.VerificarComprobanteManaged;
