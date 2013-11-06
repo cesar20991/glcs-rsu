a1123d4a9a85ab0985bed571ff2947ce076016d1
vic.nun.les@gmail.com <vic.nun.les@gmail.com@8e15e2eb-a42d-2056-9183-df07ca370b29>
2013-11-06 06:02:42 +0000

    modificando

--- GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/HojadeDespachoBean.java
+++ GLCS-RSU-PROGRAMACION/WEBEMCH/src/java/emch/controller/bean/HojadeDespachoBean.java
@@ -174,9 +174,6 @@ public class HojadeDespachoBean {
         return "nuevahojadespacho";
     }
 
-    /**
-     * @return the listadotrabajadorXcamion
-     */
     public List<TTrabajadorxcamion> getListadotrabajadorXcamion() {
         return listadotrabajadorXcamion;
     }
